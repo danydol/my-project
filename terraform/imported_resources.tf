3577b4596b956d2cf058333ed29b498878b83361
DeployAI <deployai@example.com>
2025-07-15 09:46:35 +0000

    Import cloud object: vpc vpc-07fe56c58858664ca

--- terraform/imported_resources.tf
+++ terraform/imported_resources.tf
@@ -1,11 +1,7 @@
-resource "aws_instance" "aws_ec2_new" {
-  ami = "ami-0fa88f99504362a8b"
-  associate_public_ip_address = false
-  availability_zone = "il-central-1a"
-  iam_instance_profile = ""
-  instance_type = "t3.medium"
-  key_name = "ELK-IIS_Project"
-  private_ip = "10.0.1.216"
-  subnet_id = "subnet-096c3f4df42dca912"
-  vpc_security_group_ids = ["sg-0803489b1de403fda"]
+resource "aws_vpc" "my_vpc" {
+  assign_generated_ipv6_cidr_block = false
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  instance_tenancy = "default"
 }
